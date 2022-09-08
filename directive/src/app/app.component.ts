19e07e8507aca88c3f07520f796c3eb96e729448
mcyangtw2000 <mcyangtw@gmail.com>
2022-09-08 14:32:24 +0800

    directive finished

--- directive/src/app/app.component.ts
+++ directive/src/app/app.component.ts
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-viewMode='list';
+courses=[
+  {id:1,name: 'course1'},
+  {id:2,name: 'course2'},
+  {id:3,name: 'course3'}
+];
+onAdd(){
+  this.courses.push({id:4,name:'course4'})
 }
+onRemove(course:any){
+  let index=this.courses.indexOf(course);
+  this.courses.splice(index,1);
+}
+onChange(course:any){
+  course.name='UPDATED';
+}
+}
+
  
