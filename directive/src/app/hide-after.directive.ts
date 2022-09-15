dfd82945de25e3f6569c519f4b01186d4f38659d
mcyangtw2000 <mcyangtw@gmail.com>
2022-09-15 12:34:06 +0800

    this is a delay 5 sec directive

--- directive/src/app/hide-after.directive.ts
+++ directive/src/app/hide-after.directive.ts
@@ -1,16 +1,26 @@
-import { Directive, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { InvokeFunctionExpr } from '@angular/compiler';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[appHideAfter]'
+  selector: '[hideAfter]'
 })
 export class HideAfterDirective implements OnInit {
-
+@Input('hideAfter')
+delay =0;
+@Input('hideAfterThen')
+placeholder:TemplateRef<any>|null=null;
   constructor(
     private viewContainerRef:ViewContainerRef,
      private template:TemplateRef<any>) { }
      
   ngOnInit(): void {
     this.viewContainerRef.createEmbeddedView(this.template);
+    setTimeout(()=>{
+      this.viewContainerRef.clear();
+      if(this.placeholder){
+        this.viewContainerRef.createEmbeddedView(this.placeholder);
+      }
+    },this.delay);
   }
 
 }
